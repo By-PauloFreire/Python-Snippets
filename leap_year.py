9a349e4982d6848de227a46cbef7931d8c8bdfb9
Paulo Freire <paulgarden95@gmail.com>
2022-05-27 18:51:26 -0300

    chore: Adding info to Leap Year

--- leap_year.py
+++ leap_year.py
@@ -9,6 +9,14 @@
 # approximately 365.25 days to orbit the sun. 
 # A leap year contains a leap day.
 
+# In the Gregorian calendar, three conditions are used to identify leap years: 
+# - The year can be evenly divided by 4, is a leap year, unless:
+# - The year can be evenly divided by 100, it is NOT a leap year, unless:
+# - The year is also evenly divisible by 400. Then it is a leap year.
+#
+# source: https://www.timeanddate.com/date/leapyear.html
+
+
 def leap_year(y):
     try:
         year = int(y)
